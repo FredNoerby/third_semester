fd5f4996e715699bf260212b5de3818b809cc7a9
sebastian-aau <34056722+sebastian-aau@users.noreply.github.com>
2018-10-19 12:39:54 +0200

    New environment for testing purpose

---
+++ Environment.py
@@ -0,0 +1,26 @@
+
+class Environment:
+
+
+    def __init__(self):
+
+    def _sense(self, position):
+
+
+        return new_bb
+
+
+    def _move(self, position, action):
+
+
+        return new_position
+
+
+
+    def reset(self):
+
+
+    def step(self, action):
+
+
+        return observation, reward, done, info
