21947ad32a8f710b0f50fe26bb871d630920d6f8
Frederik Nørby <frederiknoerby@outlook.dk>
2018-10-29 13:04:07 +0100

    Updated create_time_matrix script to fit new naming convention for ProjectRobot class

--- robot_control/create_time_matrix.py
+++ robot_control/create_time_matrix.py
@@ -22,23 +22,19 @@ time_matrix = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 						[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 						[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], np.int32)
 
-converter_list = [['l', 0], ['l', 1], ['l', 2], ['l', 3], ['l', 4],
-				  ['m', 0], ['m', 1], ['m', 2], ['m', 3],
-				  ['h', 0], ['h', 1], ['h', 2]]
-
 #counter = 0
 for i in range(12):
-	print("scoring for", converter_list[i][0], converter_list[i][1])
-	rob.go_to(converter_list[i][0], converter_list[i][1])
+	print("scoring for: position", str(i))
+	rob.go_to(i)
 
 	for j in range(12):
 		start_time = int(round(time.time() * 1000))
-		print("going to:", converter_list[j][0], converter_list[j][1])
-		rob.go_to(converter_list[j][0], converter_list[j][1])
+		print("going to: position", str(j))
+		rob.go_to(j)
 		time_matrix[i][j] = int(round(time.time() * 1000)) - start_time
 		print(time_matrix[i][j], 'millis')
-		print("returning to:", converter_list[i][0], converter_list[i][1])
-		rob.go_to(converter_list[i][0], converter_list[i][1])
+		print("returning to: position", str(i))
+		rob.go_to(i)
 		#counter += 1
 
 print(time_matrix)
